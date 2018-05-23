dde33488a77ebb3a33f4600ca01da7afced99912
avkoehl <avkoehl@ucdavis.edu>
2018-05-23 13:24:24 -0700

    entire wor of rebellion corpus scraped

--- api/data/war-of-rebellion/scrape.py
+++ api/data/war-of-rebellion/scrape.py
@@ -16,6 +16,10 @@ for line in indexfile:
     odir = "./" + serial + "/"
     pathlib.Path(odir).mkdir(parents=True, exist_ok=True) 
 
+    if int(serial) < 122:
+        print ("already done ... moving on")
+        continue
+
 
     ## for each serial, get data from each page
     for i in range(npages + 1):
@@ -29,9 +33,17 @@ for line in indexfile:
         source = urlopen(fullurl).read()
 
         soup = BeautifulSoup(source, 'html.parser')
-        content = soup.find("blockquote").find_all("p")
-        for p in content:
-            text = text + p.text
+        if ("Books" in soup.title.string):
+            print ("skipping page", i) # doesn't exist
+            continue
+
+        if (soup.find("blockquote")):
+            content = soup.find("blockquote").find_all("p")
+            for p in content:
+                text = text + p.text
+
+        else:
+            print ("no text found")
 
         print (text, file=ofile)
 
